bfcadb220b9deac98676660d0d5e248a4f202bf2
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2025-09-28 17:10:56 +0400

    add background and cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import MainPage from "./components/main page/MainPage";
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
       <MainPage />
     </>
   );
-}
+};
 
 export default App;
--- src/components/main page/MainPage.tsx
+++ src/components/main page/MainPage.tsx
@@ -1,7 +1,20 @@
-const MainPage = () => {
+const MainPage: React.FC = () => {
   return (
     <>
-      <p className="text-[0px]">asdasfa</p>
+      <div className="w-full h-screen ">
+        <div className="w-full h-[240px] bg-dark-background pt-[32px] pl-[17px] pr-[17px] ">
+          <div className="w-[342px] justify-self-center">
+            <img
+              src="/bg-card-back.png"
+              className="w-[286px] h-[157px] justify-self-end "
+            />
+            <img
+              src="/bg-card-front.png"
+              className="w-[286px] h-[157px] justify-self-start absolute top-[120px]"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
